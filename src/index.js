d9ff98933cf3a0a0a1def941e9bf4756708b1b72
thirota5 <t.hirota.5@gmail.com>
2020-10-24 00:09:10 +0900

    Make the Board Squares Droppable

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Board from './components/Board';
+import { observe } from './services/Game'
 
+const root = document.getElementById('root')
+
+observe((knightPosition) =>
+  ReactDOM.render(<Board knightPosition={knightPosition} />, root)
+)
+/*
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Board knightPosition={[7, 0]} />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
+*/
